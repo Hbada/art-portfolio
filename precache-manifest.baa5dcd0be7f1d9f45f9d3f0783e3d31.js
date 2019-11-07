1bd22f2a9fc09aa52b97dcf7ef79ad0f4129f348
Heidi Bada <heidibada@gmail.com>
2019-11-06 20:48:19 -0800

    Updates

---
+++ precache-manifest.baa5dcd0be7f1d9f45f9d3f0783e3d31.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b08013cf57ff9e905227fce8a6277f5",
+    "url": "/art-portfolio/index.html"
+  },
+  {
+    "revision": "3e384693e7e4bc27eff7",
+    "url": "/art-portfolio/static/css/main.ccd46f6b.chunk.css"
+  },
+  {
+    "revision": "3b22100c4908247bff65",
+    "url": "/art-portfolio/static/js/2.f1ddb267.chunk.js"
+  },
+  {
+    "revision": "3e384693e7e4bc27eff7",
+    "url": "/art-portfolio/static/js/main.0ee71866.chunk.js"
+  },
+  {
+    "revision": "1bd5069e42f5029f09f7",
+    "url": "/art-portfolio/static/js/runtime-main.bad2b735.js"
+  },
+  {
+    "revision": "d8eb51d9f010a48913f5a5727f66ff68",
+    "url": "/art-portfolio/static/media/Blue_Cat.d8eb51d9.png"
+  },
+  {
+    "revision": "06fb11504227be3831b44bd36d42f35a",
+    "url": "/art-portfolio/static/media/Cookie_Eyes.06fb1150.jpg"
+  },
+  {
+    "revision": "69e008d18cb0219bccba9933f674e006",
+    "url": "/art-portfolio/static/media/Emily_Game.69e008d1.png"
+  },
+  {
+    "revision": "068c6e2cfc6942c130d6d770b04f7967",
+    "url": "/art-portfolio/static/media/Lake_Landscape.068c6e2c.jpg"
+  },
+  {
+    "revision": "fdc8437644c0e84f9d55a55f0881e899",
+    "url": "/art-portfolio/static/media/Lamp.fdc84376.png"
+  },
+  {
+    "revision": "b078e9f06bd5584f07a88acc25da93ac",
+    "url": "/art-portfolio/static/media/Lindsay_Game.b078e9f0.png"
+  },
+  {
+    "revision": "057c96b20100c2d172b3f75f122cf755",
+    "url": "/art-portfolio/static/media/Male_Sad.057c96b2.jpg"
+  },
+  {
+    "revision": "84d7aff4ed540fb188f134134b817c3f",
+    "url": "/art-portfolio/static/media/Office_Building.84d7aff4.jpg"
+  },
+  {
+    "revision": "c0a90fa3ef784cb4602a4dd05f4e311f",
+    "url": "/art-portfolio/static/media/Savannah_Tree.c0a90fa3.png"
+  }
+]);
